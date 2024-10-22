4e8f4664789388e79aa767911a5be5f97ff7fbbd
Amadeous <amadeous@users.noreply.github.com>
2024-10-22 19:00:24 +0200

    Add more metrics

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { Token } from "./db/token.js";
 import { User } from "./db/user.js";
 
 import * as dotenv from "dotenv";
+import { metrics, ValueType } from "@opentelemetry/api";
 
 dotenv.config();
 
@@ -16,12 +17,33 @@ console.log(`Proceeding in the ${EnvConfig.network}`);
 const userRepository = AppDataSource.getRepository(User);
 const tokenRepository = AppDataSource.getRepository(Token);
 
+const meter = metrics.getMeter('userRepository');
+
 initializationDBMutex.waitForUnlock()
 .then(async () => {
 
   const tokenManager = new TokenManager(tokenRepository);
 
   const alphClient = await createAlphClient(readMnemonic, userRepository, EnvConfig.fullnode, tokenManager, EnvConfig.operator);
+
+  // Add some gauge metrics
+  meter.createObservableGauge('nb_users', {
+    description: "Current number of users of TipALPH",
+    unit: 'users',
+    valueType: ValueType.INT,
+  }).addCallback(async observableResult => {
+    const currentNbUser = await userRepository.count()
+    observableResult.observe(currentNbUser);
+  });
+
+  meter.createObservableGauge('nb_tokens', {
+    description: "Current number of registered tokens in TipALPH",
+    unit: 'tokens',
+    valueType: ValueType.INT,
+  }).addCallback(async observableResult => {
+    const currentNbToken = await tokenRepository.count()
+    observableResult.observe(currentNbToken);
+  });
 
   runTelegram(alphClient, userRepository, tokenManager);
 })
